925f6108ac46c788a9d4c7668dec15e72fb0286d
John Iacona <john.iacona@worldone.com>
2015-01-07 10:24:04 -0500

    rename some variables and remove extraneous file

--- lib/deep_slice.rb
+++ lib/deep_slice.rb
@@ -1,27 +1,29 @@
 require "deep_slice/version"
-require 'pry'
 
 class Hash
-  def deep_slice(*elements)
+  def deep_slice(*keys)
+    # use self.class so sublclasses of Hash can be deep_sliced
     hash_klass = self.class
     result = hash_klass.new
 
-    elements.each do |element|
-      if element.is_a?(hash_klass)
-        element.keys.each do |sub_element|
-          if has_key?(sub_element)
-            recursive_elements = element[sub_element]
-            result[sub_element] = if recursive_elements.is_a?(hash_klass)
-                                    self[sub_element].deep_slice(recursive_elements)
-                                  else
-                                    self[sub_element].deep_slice(*Array(recursive_elements))
-                                  end
+    keys.each do |key|
+      if key.is_a?(hash_klass)
+        key.keys.each do |sub_key|
+          if has_key?(sub_key)
+            recursive_keys = key[sub_key]
+            result[sub_key] = \
+              if recursive_keys.is_a?(hash_klass)
+                self[sub_key].deep_slice(recursive_keys)
+              else
+                self[sub_key].deep_slice(*Array(recursive_keys))
+              end
           end
         end
-      elsif has_key?(element)
-        result[element] = self[element]
+      elsif has_key?(key)
+        result[key] = self[key]
       end
     end
+
     result
   end
 end
